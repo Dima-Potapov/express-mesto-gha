0e21f06aea560d3a3c446f8e8ec447a9fa093777
Dmitriy Potapov <dima.potapov1994@gmail.com>
2023-01-15 23:35:17 +0300

    edit 14 work

--- app.js
+++ app.js
@@ -22,7 +22,7 @@ app.use(cookieParser());
 
 app.use(router);
 
-app.use(errorHandler);
 app.use(errors());
+app.use(errorHandler);
 
 app.listen(PORT, () => console.log(`App listening on port ${PORT}`));
